a7360af3ac0159b8cf6a1b2bc808b89048ff2666
superznake <superznake@yandex.ru>
2024-10-08 12:49:06 +0300

    .

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((600, 600))
 clock = pygame.time.Clock()
 running = True
 dt = 0
